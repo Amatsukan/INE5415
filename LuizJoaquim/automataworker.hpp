cc33ad3c33f4e126a3bfa17c11ea296281f3670a
putschini <joaquimbbelo@gmail.com>
2016-05-22 23:04:52 -0300

    AutomataWorker falta correcao

---
+++ LuizJoaquim/automataworker.hpp
@@ -0,0 +1,19 @@
+#ifndef AW_H
+#define AW_H
+
+#include "automata.hpp"
+
+#include <set> 
+
+template <typename Symbol_type>
+
+class AutomataWorker
+{
+public:
+
+	Automata<char> determiniza(Automata<char>);
+	
+};
+
+
+#endif
